559028c19a9ac2474ac0cf8e33a3d6ef2e7c5fb2
Machfar-Souhayel <souhayel.isetch@gmail.com>
2019-11-13 18:25:25 +0100

    Nice Grid

---
+++ js/main.js
@@ -0,0 +1,6 @@
+troll = document.getElementsByClassName("troll-section")[0];
+btn = document.getElementsByClassName("poop")[0];
+
+btn.addEventListener("click", function(){
+	troll.style.display="none"
+});
